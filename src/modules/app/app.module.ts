3b68d7c8d8988fea2a643a2760cd539dfdb5709d
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2026-01-22 16:24:15 +0330

    feat: add typeorm config

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,11 +3,17 @@ import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeormConfig } from "src/config/typeorm.config";
 
 @Module({
   providers: [AppService],
   controllers: [AppController],
   imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfig,
+      inject: [TypeormConfig],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(process.cwd(), ".env"),
