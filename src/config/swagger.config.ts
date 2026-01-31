9f4e2dd6959f2d0d859fde9ef0103acc74a171c5
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2026-01-31 13:40:19 +0330

    feat: add generate app version script based on package.json file

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,3 +1,4 @@
+import { App } from "../common/enums/app.enum";
 import type { INestApplication } from "@nestjs/common";
 import { ApiHeader } from "src/common/enums/header.enum";
 import type { SecuritySchemeObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
@@ -27,7 +28,7 @@ const globalResponses: ApiResponseOptions[] = [
 function configureSwagger(app: INestApplication): void {
   const document = new DocumentBuilder()
     .setTitle("Virgool")
-    .setVersion("0.0.1")
+    .setVersion(App.VERSION)
     .setOpenAPIVersion("3.1.0")
     .addGlobalResponse(...globalResponses)
     .setDescription("Backend of virgool website")
