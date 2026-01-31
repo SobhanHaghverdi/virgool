ad9253bd224e55cd18e51e5fd3c1712ccf838de2
SobhanHaghverdi <sobhanhv.dev@gmail.com>
2026-01-31 14:53:30 +0330

    refactor: delete app controller and service

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,12 @@
 import { join } from "path";
 import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
 import UserModule from "../user/user.module";
 import AuthModule from "../auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
 import TypeormConfig from "src/config/typeorm.config";
 
 @Module({
-  providers: [AppService],
-  controllers: [AppController],
   imports: [
     AuthModule,
     UserModule,
